740ac050f22681a7bd6479d1554d8e37283f1724
Joerg Heinrich <contact@jfheinrich.eu>
2025-07-23 07:35:14 +0200

    fix: Refactor test-docker-build.sh to use variable for builder name for consistency

--- test-docker-build.sh
+++ test-docker-build.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
 # Tested with Rancher Desktop 1.19.3 and Docker 28.1.1-rd
-
+BUILDER_NAME="mybuilder"
 docker run --privileged --rm tonistiigi/binfmt --install all >/dev/null
 
-builder="$(docker buildx ls 2>/dev/null | grep mybuilder)"
+builder="$(docker buildx ls 2>/dev/null | grep $BUILDER_NAME)"
 if [ -z "$builder" ]; then
-    echo "Creating a new buildx builder named 'mybuilder'."
-    docker buildx create --name mybuilder --use --driver docker-container
+    echo "Creating a new buildx builder named '$BUILDER_NAME'."
+    docker buildx create --name $BUILDER_NAME --use --driver docker-container
 else
     echo "Using existing buildx builder: $builder"
-    docker buildx use mybuilder
+    docker buildx use $BUILDER_NAME
 fi
 
-docker buildx build \
-    --platform linux/amd64,linux/arm64 \
-    --tag testbuild:buildx-latest \
-    -f Dockerfile-daily-report .
+if [ -z "$1" ] || [ "$1" = "multiarch" ]; then
+    docker buildx build \
+        --platform linux/amd64,linux/arm64 \
+        --tag testbuild:buildx-latest \
+        -f Dockerfile-daily-report .
+fi
 
-docker build --tag testbuild:docker-latest -f Dockerfile-daily-report .
+if [ -n "$1" ] && [ "$1" = "single" ]; then
+    docker build --tag testbuild:docker-latest -f Dockerfile-daily-report .
+fi
